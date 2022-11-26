24534448edc741df02f09d0f3911fa87012831d9
EC2 Default User <ec2-user@ip-172-31-8-245.ap-northeast-1.compute.internal>
2022-11-26 13:30:30 +0000

    [update]11262230閲覧申請完了画面作成・管理者側閲覧申請一覧画面作成完了

--- app/controllers/admin/view_applications_controller.rb
+++ app/controllers/admin/view_applications_controller.rb
@@ -1,13 +1,24 @@
 class Admin::ViewApplicationsController < ApplicationController
   def index
+    @view_applications = ViewApplication.all
+    @view_application = ViewApplication.new
+    @properties = Property.all
+    @users = User.all
   end
 
   def edit
+    @view_application = ViewApplication.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def view_application_params
+    params.require(:view_application).permit(:user_id, :property_id, :apply_status)
   end
 end
