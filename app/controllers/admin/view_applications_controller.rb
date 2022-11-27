2f0cf36b56046e7e8111b9215ceb8f287a595382
EC2 Default User <ec2-user@ip-172-31-8-245.ap-northeast-1.compute.internal>
2022-11-27 12:17:39 +0000

    [update]11272117閲覧申請一覧画面修正・申請編集画面作成完了

--- app/controllers/admin/view_applications_controller.rb
+++ app/controllers/admin/view_applications_controller.rb
@@ -7,13 +7,25 @@ class Admin::ViewApplicationsController < ApplicationController
   end
 
   def edit
+    @properties = Property.all
     @view_application = ViewApplication.find(params[:id])
   end
 
   def update
+    @view_application = ViewApplication.find(params[:id])
+    if @view_application.update(property_params)
+      flash[:notice] = "変更を保存しました。"
+      redirect_to admin_view_application_path(@view_application)
+    else
+      flash[:arlet] = "変更の保存に失敗しました。"
+      render:edit
+    end
   end
 
   def destroy
+    @view_application = ViewApplication.find(params[:id])
+    @view_application.destroy
+    redirect_to admin_view_applications_path, notice: "閲覧申請を削除しました。"
   end
 
   private
