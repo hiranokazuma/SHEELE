147afcf47e1a9e3e3da2848d38991cf6c82cc86d
EC2 Default User <ec2-user@ip-172-31-8-245.ap-northeast-1.compute.internal>
2022-12-03 14:44:56 +0000

    [update]12032344会員側登録情報編集画面/登録情報詳細画面作成

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,17 +3,42 @@ class Public::UsersController < ApplicationController
   end
 
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:notice] = "変更を保存しました。"
+    else
+      flash[:arlet] = "変更に失敗しました。"
+      render :edit
+    end
   end
 
   def unsubscribe
+    @user = current_user
   end
 
   def withdraw
+    @user = current_user
+    # is_deletedカラムをtrueに変更することにより削除フラグを立てる
+    @user.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行しました。"
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:company_name, :location, :licensing_entity,
+                                :licensing_region, :update_number, :license_number,
+                                :representatives_name, :telephone_number, :manager_name,
+                                :is_deleted, :email, :password, :password_confirmation)
   end
 end
