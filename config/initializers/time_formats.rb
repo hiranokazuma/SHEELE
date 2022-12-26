3bdb05dff903b54100d496947a355ae9887bfd70
EC2 Default User <ec2-user@ip-172-31-8-245.ap-northeast-1.compute.internal>
2022-12-26 15:00:13 +0000

    [update]12262359アイコン配置

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,5 +1,5 @@
 
-Time::DATE_FORMATS[:date_jp] = "%Y年 %m月 %d日"
-Time::DATE_FORMATS[:time_jp] = '%H時 %M分 %S秒'
+Time::DATE_FORMATS[:date_jp] = "%Y年%m月%d日"
+Time::DATE_FORMATS[:time_jp] = '%H時%M分%S秒'
 Time::DATE_FORMATS[:datetime_jp] = '%Y年%m月%d日' + "\n" + '%H時%M分%S秒'
 
