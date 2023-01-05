7c86b4e810a53ecb0172a68678c57470e2f2e0a7
EC2 Default User <ec2-user@ip-172-31-8-245.ap-northeast-1.compute.internal>
2023-01-05 15:24:00 +0000

    [update01060023]テスト＆修正中(通知機能/閲覧申請表示)

--- app/controllers/public/management_notices_controller.rb
+++ app/controllers/public/management_notices_controller.rb
@@ -1,5 +1,6 @@
 class Public::ManagementNoticesController < ApplicationController
   def show
     @management_notice = ManagementNotice.find(params[:id])
+    @management_notice.notifications.update(read: true)
   end
 end
--- app/controllers/public/view_applications_controller.rb
+++ app/controllers/public/view_applications_controller.rb
@@ -2,9 +2,10 @@ class Public::ViewApplicationsController < ApplicationController
   before_action :set_property, only: [:confirm, :create, :complete]
 
   def index
-    @view_applications = ViewApplication.where.not(user_id: current_user.id)
     # @view_applications = ViewApplication.where.not(user_id: current_user.id)
-    @property = Property.find_by(user_id: current_user.id)
+    # @view_applications = ViewApplication.where.not(user_id: current_user.id)
+    # @property = Property.find_by(user_id: current_user.id)
+    @applied_properties = Property.joins(:view_applications).where(user_id: current_user.id).where.not(view_applications:{user_id: current_user.id}).distinct
     # @view_application = ViewApplication.new
     @properties = current_user.view_application_properties.select_status("申請中", "許可")
     @user = current_user.id
