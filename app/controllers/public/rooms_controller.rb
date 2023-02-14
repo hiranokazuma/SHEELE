f2c3054ea3bc3cf5ab8a8e3fea97a9fe53fd683b
EC2 Default User <ec2-user@ip-172-31-19-175.ap-northeast-1.compute.internal>
2023-02-14 17:08:04 +0000

    [update]02150207会員間メッセージ機能作成７０％

--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -1,6 +1,6 @@
 class Public::RoomsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     @room = Room.create
     @joinCurrentUser = RoomUser.create(user_id: current_user.id, room_id: @room.id)
@@ -19,9 +19,9 @@ class Public::RoomsController < ApplicationController
       redirect_back(fallback_location: property_path(@property))
     end
   end
-  
+
   private
-  
+
   def join_room_params
     params.require(:room_user).permit(:user_id, :room_id).merge(room_id: @room.id)
   end
