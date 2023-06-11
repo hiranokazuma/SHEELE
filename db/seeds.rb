649c9042e1a2c7891a3272026577080990e30e0f
KazumaHirano <110404855+hiranokazuma@users.noreply.github.com>
2023-06-11 17:18:35 +0000

    初期テストコード作成中

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,14 @@ Admin.create!(
   )
 #　管理者ログインページ
 # https://sheele.site/admin/sign_in
+
+users = User.create!(
+  [
+    {company_name: "星野不動産㈱", location: "東京都千代田区有楽町０―０―０", licensing_entity: "国土交通大臣", licensing_region: , update_number: "1", license_number: "99999",
+    representatives_name: "星野太郎",telephone_number: "0123456789", manager_name: "星野次郎", email: 'hoshino-re@test.com', password: 'password' },
+    {company_name: "㈱トワイライトエステート", location: "東京都渋谷区代々木０―０―０", licensing_entity: "知事", licensing_region: "東京都", update_number: "5", license_number: "999999",
+    representatives_name: "黄昏太郎",telephone_number: "1234567890", manager_name: "黄昏次郎", email: 'twilight-re@test.com', password: 'password'},
+    {company_name: "天の川不動産㈱", location: "東京都港区南青山０―０―０", licensing_entity: "知事", licensing_region: "東京都", update_number: "10", license_number: "888888",
+    representatives_name: "天川太郎",telephone_number: "0987654321", manager_name: "天川次郎", email: 'milkyway-re@test.com', password: 'password'}
+  ]
+)
